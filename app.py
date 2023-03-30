4bbbb0b82807b25c368bbc755d08fd847bd53ccd
Salah Ashraf <salahashraf924@gmail.com>
2023-03-30 23:17:48 +0200

    add endpoint for hotel recommender

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-
+from Hotels import test_env
+from Hotels.test_env import recommend_hotel
+import jsonpickle
 app = Flask(__name__)
 
 
@@ -8,11 +10,15 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/hotel', methods=['POST'])
-def hotel_recommednation():
-    print(request.get_json())
-    return "Hotel Recommender"
-
+@app.route('/hotel/<userId>/<city>', methods=['POST'])
+def hotel_recommednation(userId,city):
+    assert userId == request.view_args['userId']
+    assert city == request.view_args['city']
+    df = recommend_hotel(userId, city)
+    print(df)
+    return df
 
 if __name__ == '__main__':
     app.run(port=5000)
+
+
