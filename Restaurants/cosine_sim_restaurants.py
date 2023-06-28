dc3f2a65f1822b2896158b4c0ec666152bbfbc0d
RawanGamalElDin12 <rawan.gamaleldin@outlook.com>
2023-06-28 16:52:33 +0200

    Update cosine_sim_restaurants.py

--- Restaurants/cosine_sim_restaurants.py
+++ Restaurants/cosine_sim_restaurants.py
@@ -8,47 +8,44 @@ df = pd.read_csv(r"C:\Users\Salah Ashraf\PycharmProjects\TravelRecommeder_Flask\
 
 #print(df.head())
 
+# fill missing values in the "cuisines" column
 df["cuisines"].fillna("", inplace=True)
 
+# create a CountVectorizer object to transform the "cuisines" column into a matrix of word counts
 cv = CountVectorizer(stop_words='english')
 count_matrix = cv.fit_transform(df["cuisines"])
-#print("Count Matrix:", count_matrix.toarray())
-#print(cv.get_stop_words(count_matrix))
-#print(cv.get_feature_names_out(count_matrix))
-# compute the cosine similarity matrix
+
+# compute the cosine similarity matrix between all pairs of restaurants based on their cuisine
 similarity = cosine_similarity(count_matrix)
-#print(similarity)
 
-
-
-# create a function that takes in restaurant name as input and returns a list of the most similar restaurants
+# create a function that takes in restaurant ID and number of recommendations as inputs and returns a list of the most similar restaurants in the same city
 def get_recommendations(restaurant_id, n=20, cosine_sim=similarity):
-    # get the index of the restaurants that matches the restaurant name
-    restaurant_index =restaurant_id
+    # get the city of the restaurant
+    city = df[df['restID'] == restaurant_id]['city'].values[0]
 
-    # get the pairwsie similarity scores of all restaurants with that restaurant and sort the restaurant based on the similarity scores
+    # select only the restaurants that match the same city
+    df_city = df[df['city'] == city]
+
+    # get the index of the restaurant that matches the restaurant ID
+    restaurant_index = df_city.index[df_city['restID'] == restaurant_id].tolist()[0]
+
+    # get the pairwise similarity scores of all restaurants in the same city and sort the restaurants based on the similarity scores
     sim_scores_all = sorted(list(enumerate(cosine_sim[restaurant_index])), key=lambda x: x[1], reverse=True)
 
-    # checks if recommendations are limited
+    # filter the restaurants by city
+    sim_scores_city = [(i, score) for i, score in sim_scores_all if df.iloc[i]['city'] == city]
+
+    # check if recommendations are limited
     if n > 0:
-        sim_scores_all = sim_scores_all[1:n + 1]
+        sim_scores_city = sim_scores_city[1:n + 1]
 
     # get the restaurant indices of the top similar restaurants
-    restaurants_indices = [i[0] for i in sim_scores_all]
-    scores = [i[1] for i in sim_scores_all]
-    # for i in restaurants_indices:
-    #     print( i)
-    # return the top n most similar restaurants from the restaurants df
-    top_restaurants = pd.DataFrame(df.iloc[restaurants_indices]['name'])
+    restaurants_indices = [i[0] for i in sim_scores_city]
+    scores = [i[1] for i in sim_scores_city]
+
+    # create a DataFrame of the top similar restaurants and their similarity scores
+    top_restaurants = pd.DataFrame(df.iloc[restaurants_indices][['restID', 'name', 'city']])
     top_restaurants['sim_scores'] = scores
     top_restaurants['ranking'] = range(1, len(top_restaurants) + 1)
-    return restaurants_indices
-    # return top_restaurants, sim_scores_all
-
-
-# # generate a list of recommendations for a specific restaurant name
-# restaurant_name = 'Beeja'
-# top_restaurants_df, _ = get_recommendations(restaurant_name, 20)
-
-
+    return top_restaurants['restID'].tolist()
 
